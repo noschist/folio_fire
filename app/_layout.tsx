864c9e22a17eb88594c7b0522d473b2828129f77
noschist <sreenair1999@gmail.com>
2024-06-26 19:04:05 +0530

    Installed Tailwind

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+import "../global.css";
+
+const RootLayout = () => {
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
+};
+
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View className="flex flex-1 justify-center items-center bg-black text-white">
+            <Text>Hello World</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
